8cdb421dddc80b0b1e24bdbf2fb3e396653944cc
gaozhiqiang <1211348968@qq.com>
2019-03-19 02:17:17 -0400

    test

---
+++ kubeadm_components.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+VERSION=v1.12.2
+MASTER_COMPONENTS='k8s.gcr.io/kube-proxy:v1.12.2
+k8s.gcr.io/kube-apiserver:v1.12.2
+k8s.gcr.io/kube-controller-manager:v1.12.2
+k8s.gcr.io/kube-scheduler:v1.12.2
+k8s.gcr.io/etcd:3.2.24
+k8s.gcr.io/coredns:1.2.2
+quay.io/coreos/flannel:v0.10.0-amd64
+k8s.gcr.io/pause:3.1'
+NODE_COMPONENTS='k8s.gcr.io/kube-proxy:v1.12.2
+quay.io/coreos/flannel:v0.10.0-amd64
+k8s.gcr.io/pause:3.1'
+
+git_init(){
+	git config --global user.name "gaozhiqiang"
+	git config --golbal user.email "1211348968@qq.com"
+	git remote remove origin
+	git remote add origin git@github.com:solomonlinux/kubernetes_components.git
+	if git branch -a | grep 'origin/develop' &> /dev/null; then
+		git checkout develop
+		git pull origin develop
+		git branch --set-upstream-to=origin/develop develop
+	else
+		git checkout -b develop
+		git pull origin develop
+	fi
+}
+
+git_commit(){
+	git status -s | wc -l
+}
+
+pull_images(){
+	echo
+	for IMAGE $MASTER_COMPONENTS; do
+		docker pull $IMAGE
+	done
+	docker save -o kubernetes-${VERSION}-master-components.tar.gz $MASTER_COMPONENTS
+	docker save -o kubernetes-${VERSION}-node-components.tar.gz $NODE_COMPONENTS
+}
+
+main(){
+	git_init
+	pull_images
+	git_commit
+}
+
+main
