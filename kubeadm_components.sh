8df5fe47cf33fb79d69ae985cec558e2b1cd149c
gaozhiqiang <1211348968@qq.com>
2019-03-19 15:09:07 -0400

    test

--- kubeadm_components.sh
+++ kubeadm_components.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-VERSION=v1.12.2
-MASTER_COMPONENTS='k8s.gcr.io/kube-proxy:v1.12.2
-k8s.gcr.io/kube-apiserver:v1.12.2
-k8s.gcr.io/kube-controller-manager:v1.12.2
-k8s.gcr.io/kube-scheduler:v1.12.2
-k8s.gcr.io/etcd:3.2.24
-k8s.gcr.io/coredns:1.2.2
-quay.io/coreos/flannel:v0.10.0-amd64
-k8s.gcr.io/pause:3.1'
-NODE_COMPONENTS='k8s.gcr.io/kube-proxy:v1.12.2
-quay.io/coreos/flannel:v0.10.0-amd64
-k8s.gcr.io/pause:3.1'
-git_lfs_init(){
-	git lfs install --force
-	git lfs track "*.tar.gz"
+DOCKERHUB_REPO_NAME=solomonlinux
+#source kubeadm_pods.txt
+IMAGE_LIST_FILE=kubeadm_pod.txt
+
+
+# 安装依赖的软件包
+app_install(){
+	echo
 }
 
+# github仓库初始化
 git_init(){
-	#git config --global user.name "gaozhiqiang"
-	git config --global user.name "solomonlinux"
+	git config --global user.name "gaozhiqiang"
 	git config --global user.email "1211348968@qq.com"
 	git remote remove origin
 	git remote add origin git@github.com:solomonlinux/kubernetes_components.git
@@ -33,9 +26,8 @@ git_init(){
 	fi
 }
 
+# github仓库提交
 git_commit(){
-	echo kubernetes-${VERSION}-master-components.tar.gz > .gitignore
-	echo kubernetes-${VERSION}-node-components.tar.gz > .gitignore
 	local COMMIT_FILES_COUNT=$(git status -s | wc -l)
 	local TODAY=$(date "+%F %T")
 	if [ $COMMIT_FILES_COUNT -gt 0 ]; then
@@ -45,22 +37,61 @@ git_commit(){
 	fi
 }
 
-pull_images(){
-	echo
-	for IMAGE in $MASTER_COMPONENTS; do
-		docker pull $IMAGE
-	done
-	docker save -o kubernetes-${VERSION}-master-components.tar.gz $MASTER_COMPONENTS
-	docker save -o kubernetes-${VERSION}-node-components.tar.gz $NODE_COMPONENTS
-	mv kubernetes-${VERSION}-master-components.tar.gz images/
-	mv kubernetes-${VERSION}-node-components.tar.gz images/
+# 为了检查用户输入的镜像,如k8s.gcr.io/kube-proxy:v1.12.2转换为solomonlinux/gcr.io.kube-proxy:v1.12.2,用于检查我自己的仓库有没有
+# $1为用户提供的镜像名称,如k8s.gcr.io/kube-proxy:v1.12.2
+image_tag_convert(){
+	local SRC_IMAGE=$1
+	local DOMAIN=${SRC_IMAGE%%/*}
+	if [ $DOMAIN == "k8s.gcr.io" ]; then
+        	DEST_IMAGE=${DOCKERHUB}/`echo ${SRC_IMAGE#*.} | tr / .`
+		echo $DEST_IMAGE
+	elif [ $DOMAIN == 'gcr.io' ]; then
+        	DEST_IMAGE=${DOCKERHUB}/`echo ${SRC_IMAGE} | tr / .`
+		echo $DEST_IMAGE
+	elif [ $DOMAIN == 'quay.io' ]; then
+        	DEST_IMAGE=${DOCKERHUB}/`echo $SRC_IMAGE | tr / .`
+		echo $DEST_IMAGE
+	else
+        	DEST_IMAGE=$SRC_IMAGE
+		echo $DEST_IMAGE
+	fi
+}
+
+# 用于检查我自己的仓库是否存在镜像
+# $1为镜像名称,如nginx,$2为镜像标签,如v1-test
+# 返回值为0表示镜像存在,为1表示镜像不存在
+# $1: image_name; $2: image_tag_name
+image_tag_check(){
+        local RESULT=$(curl -s https://hub.docker.com/v2/repositories/${DOCKERHUB_REPO_NAME}/$1/tags/$2/ | jq -r .name)
+	if [ $RESULT == null ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+
+# 检查我自己的仓库是否有镜像,有就算了,没有就拉取并改名上传至自己的仓库
+# xxx为镜像列表
+pull_image(){
+
+	while read IMAGE TAG; do
+		echo "$IMAGE ---- $TAG"
+		local DEST_IMAGE=$(image_tag_convert ${IMAGE}:${TAG})
+		image_tag_check ${IMAGE} ${TAG}
+		if [ $? -ne 0 ]; then
+			docker pull ${IMAGE}:${TAG}
+			docker push ${DOCKERHUB_REPO_NAME}/${IMAGE#*/}:${TAG}
+		fi
+	done < <( cat $IMAGE_LIST_FILE | grep -v "^$" | awk -F: '/:/{print $1,$2}' )
 }
 
 main(){
-	git_lfs_init
+	app_install
 	git_init
-	pull_images
+	pull_image
 	git_commit
 }
 
+# 我使用git lfs报错,而且它的容量为1G,因为我想通过脚本来实现我的仓库里必须有对应的镜像存在,并且在使用时通过脚本能够快速拉取所需镜像
 main
