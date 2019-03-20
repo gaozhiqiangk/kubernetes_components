c2381790e08e9a439a5d5904fd17c28f7a6441aa
gaozhiqiang <1211348968@qq.com>
2019-03-20 01:23:56 -0400

    test

--- kubeadm_components.sh
+++ kubeadm_components.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 DOCKERHUB_REPO_NAME=solomonlinux
-#source kubeadm_pods.txt
 IMAGE_LIST_FILE=kubeadm_components.txt
 
 set -e
 
 # 安装依赖的软件包
 app_install(){
-	echo
+	if ! which jq &> /dev/null; then
+		if ls /etc/ | grep 'redhat-release' &> /dev/null; then 
+			yum -y install epel-release
+			yum -y install jq
+		else
+			apt-get -y install jq
+		fi
+	fi
 }
 
 # github仓库初始化
@@ -42,20 +48,8 @@ git_commit(){
 # $1为用户提供的镜像名称,如k8s.gcr.io/kube-proxy:v1.12.2
 image_tag_convert(){
 	local SRC_IMAGE=$1
-	local DOMAIN=${SRC_IMAGE%%/*}
-	if [ $DOMAIN == "k8s.gcr.io" ]; then
-        	DEST_IMAGE="${DOCKERHUB_REPO_NAME}/`echo ${SRC_IMAGE#*.} | tr / .`"
-		echo $DEST_IMAGE
-	elif [ $DOMAIN == 'gcr.io' ]; then
-        	DEST_IMAGE="${DOCKERHUB_REPO_NAME}/`echo ${SRC_IMAGE} | tr / .`"
-		echo $DEST_IMAGE
-	elif [ $DOMAIN == 'quay.io' ]; then
-        	DEST_IMAGE="${DOCKERHUB_REPO_NAME}/`echo $SRC_IMAGE | tr / .`"
-		echo $DEST_IMAGE
-	else
-        	DEST_IMAGE=$SRC_IMAGE
-		echo $DEST_IMAGE
-	fi
+	local DEST_IMAGE="${DOCKERHUB_REPO_NAME}/${SRC_IMAGE} | tr '/' '.'"
+	echo $DEST_IMAGE
 }
 
 # 用于检查我自己的仓库是否存在镜像
@@ -63,10 +57,7 @@ image_tag_convert(){
 # 返回值为0表示镜像存在,为1表示镜像不存在
 # $1: image_name; $2: image_tag_name
 image_tag_check(){
-	#echo $1 --------
-	#echo $2 ---------
         local RESULT=$(curl -s https://hub.docker.com/v2/repositories/${DOCKERHUB_REPO_NAME}/$1/tags/$2/ | jq -r .name)
-	#echo "null"-----------------------------------------
 	if [ "$RESULT" == "null" ]; then
 		echo failure
 	else
@@ -74,38 +65,34 @@ image_tag_check(){
 	fi
 }
 
-
-# 检查我自己的仓库是否有镜像,有就算了,没有就拉取并改名上传至自己的仓库
-# xxx为镜像列表
-pull_image(){
+sync_image(){
 	while read IMAGE TAG; do
-		local SRC_IMAGE=${IMAGE}:${TAG}
-		local DEST_IMAGE=$(image_tag_convert ${SRC_IMAGE})
-		local STATUS=$(image_tag_check $(echo "${IMAGE#*/} | tr '/' '.'") ${TAG})
-		if [ $STATUS == 'failure' ]; then
-			echo "${IMAGE}:${TAG}镜像正在被拉取"
+		SRC_IMAGE=${IMAGE}:${TAG}
+		DEST_IMAGE=$(image_tag_convert $SRC_IMAGE)
+		if [ $(image_tag_check $IMAGE $TAGE) == "failure" ]; then
 			docker pull $SRC_IMAGE &> /dev/null
 			docker tag $SRC_IMAGE $DEST_IMAGE &> /dev/null
 			docker push $DEST_IMAGE &> /dev/null
-			[ $? -eq 0 ] && echo "${SRC_IMAGE}镜像已经上传至本地仓库"
+			[ $? -eq 0 ] && echo "${SRC_IMAGE}已同步完成"
 		else
-			echo "${SRC_IMAGE}镜像已存在"
+			echo "${SRC_IMAGE}镜像已存在,不需要再次同步"
 		fi
-	# 'xyz		xyz
-	# ab		ab
-	# cde		cde
-	# xyz'		xyz
-	# cat test | tr "'" "\n" | grep -v "^$" | sort | uniq
 	done < <( cat $IMAGE_LIST_FILE | tr "'" "\n" | grep -v "^$" | awk -F: '/:/{print $1,$2}' )
 }
 
 main(){
 	app_install
 	git_init
-	pull_image
+	sync_image
 	git_commit
 }
 
-# 我使用git lfs报错,而且它的容量为1G,因为我想通过脚本来实现我的仓库里必须有对应的镜像存在,并且在使用时通过脚本能够快速拉取所需镜像
-# 脚本还不够健壮,对于docker官方提供的镜像就没辙了,但是确实也不应该使用这个脚本这么干,太缺德了
 main
+
+
+# 'xyz		xyz
+# ab		ab
+# cde		cde
+# xyz'		xyz
+# cat test | tr "'" "\n" | grep -v "^$" | sort | uniq
+# ( cat $IMAGE_LIST_FILE | tr "'" "\n" | grep -v "^$" | awk -F: '/:/{print $1,$2}' )
